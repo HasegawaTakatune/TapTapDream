91f75361382a0261814d1d2156ee1ee0a267bdec
Takatune <0ta5274738t4p4r@gmail.com>
2020-08-12 23:29:36 +0900

    メニュー画面の設定画面を作成

---
+++ TapTapDream/Assets/Scripts/Menu/Configuration.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Configuration : MonoBehaviour
+{
+    private GameObject ConfigPanel = default;
+    private GameObject CreditPanel = default;
+
+    private Slider AudioSlider = default;
+    private Slider SESlider = default;
+
+    void Start()
+    {
+        Transform trns = transform.Find("ConfigPanel");
+        AudioSlider = trns.Find("AudioSlider").GetComponent<Slider>();
+        SESlider = trns.Find("SESlider").GetComponent<Slider>();
+
+        Button CreditBtn = trns.Find("CreditButton").GetComponent<Button>();
+        CreditBtn.onClick.AddListener(OnClickCreditButton);
+
+        Button ReturnBtn = trns.Find("ReturnButton").GetComponent<Button>();
+        ReturnBtn.onClick.AddListener(OnClickReturnButton);
+
+        Button CloseBtn = transform.Find("CloseButton").GetComponent<Button>();
+        CloseBtn.onClick.AddListener(OnClickCloseButton);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnClickBackgroundPanel()
+    {
+        ConfigPanel.SetActive(true);
+        CreditPanel.SetActive(false);
+        gameObject.SetActive(false);
+    }
+
+    public void OnClickCreditButton()
+    {
+        ConfigPanel.SetActive(false);
+        CreditPanel.SetActive(true);
+    }
+
+    public void OnClickReturnButton()
+    {
+        ConfigPanel.SetActive(true);
+        CreditPanel.SetActive(false);
+    }
+
+    public void OnClickCloseButton()
+    {
+        ConfigPanel.SetActive(true);
+        CreditPanel.SetActive(false);
+        gameObject.SetActive(false);
+    }
+}
---
+++ TapTapDream/Assets/Scripts/Menu/StatusItem.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusItem : MonoBehaviour
+{
+    private int maxParam = 20;
+
+    private Text paramText = default;
+
+    private int param = 0;
+
+    [SerializeField] private string label;
+
+    public void Init(int param, int maxParam)
+    {
+        this.param = param;
+        this.maxParam = maxParam;
+    }
+
+    private void Start()
+    {
+        Text labelTxt = transform.Find("ParamPanel").Find("Label").GetComponent<Text>();
+        labelTxt.text = label;
+
+        paramText = transform.Find("ParamPanel").Find("Param").GetComponent<Text>();
+
+        Button addBtn = transform.Find("AddButton").GetComponent<Button>();
+        Button subBtn = transform.Find("SubButton").GetComponent<Button>();
+
+        addBtn.onClick.AddListener(OnClickAddParamButton);
+        subBtn.onClick.AddListener(OnClickSubParamButton);
+    }
+
+    public void OnClickAddParamButton()
+    {
+        if (param < maxParam)
+            param++;
+
+        paramText.text = param.ToString();
+    }
+
+    public void OnClickSubParamButton()
+    {
+        if (0 < param)
+            param--;
+
+        paramText.text = param.ToString();
+    }
+
+    public int GetParam() { return param; }
+}
